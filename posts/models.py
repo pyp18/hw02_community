682d6b155650dd75e61c82e3cc1fbf7bef4963e7
Никита <pyp18@yandex.ru>
2020-10-08 18:37:33 +0300

    Fix 1

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=40, unique=True)
     description = models.TextField()
 
     def __str__(self):
@@ -20,3 +20,6 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL, blank=True, null=True, related_name="posts")
+
+    class Meta:
+        ordering = ['-pub_date']
